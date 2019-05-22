d058d36fc9f0755fc98f0246bcc6d99ea0971757
Efthymios Tzinis <etzinis@gmail.com>
2019-05-22 16:18:29 -0400

    Tasnet model wrapper/orchestrator

--- neural_dEMOmpose/models/tasnet/tasnet.py
+++ neural_dEMOmpose/models/tasnet/tasnet.py
@@ -1,7 +1,14 @@
+"""!
+@brief TasNet Wrapper 
+@todo Convert it for classification purposes
+
+@author Efthymios Tzinis {etzinis2@illinois.edu}
+@copyright University of Illinois at Urbana Champaign
+"""
+
 import time, sys, os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 root_dir = os.path.join(
    os.path.dirname(os.path.realpath(__file__)),
    '../../../')
@@ -28,7 +35,8 @@ class TasNet(nn.Module):
             B: int, number of out channels in the bottleneck layer
             H: int, number of out channels in D-conv layer 
             P: int, size of D-conv filter 
-            X: int, number of conv blocks in each repeat (max depth of dilation)
+            X: int, number of conv blocks in each repeat (max depth
+                    of dilations)
             R: int, number of repeats
 
     '''
@@ -37,13 +45,15 @@ class TasNet(nn.Module):
         self.encoder = EncodingLayer(N, L)
         self.sep_module = SeparationModule(N, B, H, P, X, R, C)
         self.decoder = DecodingLayer(N, L)
-        #  self.decoder = nn.ConvTranspose2d(C, C, (N, L), stride=(1, L//2), groups=C)
+        #  self.decoder = nn.ConvTranspose2d(C, C, (N, L),
+        #                 stride=(1, L//2), groups=C)
 
     def forward(self, x):
         x = self.encoder(x)
         m_out = self.sep_module(x)
         #  x_hat = self.decoder(m_out)
-        x_hat = torch.cat([self.decoder(m_out[:, c, :, :]) for c in range(m_out.shape[1])], dim=1)
+        x_hat = torch.cat([self.decoder(m_out[:, c, :, :])
+                           for c in range(m_out.shape[1])], dim=1)
         return x_hat
 
 
@@ -57,10 +67,11 @@ if __name__ == '__main__':
     R = 4
     C = 2
 
-    device = 'cuda:2'
+    device = 'cuda:0'
     sig = torch.randn(4, 1, 32000).to(device)
     model = TasNet(N, L, B, H, P, X, R, C).to(device)
-    print('==> Number of trainable params: {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+    print('==> Number of trainable params: {}'.format(sum(p.numel()
+          for p in model.parameters() if p.requires_grad)))
     t0 = time.time()
     out = model(sig)
     print(time.time()-t0)
